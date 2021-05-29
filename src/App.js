11cb101e3dcfdd3dee130c68a72d5bcddffc89ae
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-05-29 23:12:44 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -4,17 +4,6 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: true
-    };
-  }
-
-  componentDidMount(){
-    this.setState({loading: false});
-  }
-
   render() {
     return (
       <div className="container">
